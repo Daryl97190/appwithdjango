f7b6426db9f8f5cbe2e48f1c63dee9f7c7cd3c2b
Daryl Livio <daryl.livio@gmail.com>
2022-09-02 11:37:28 +0200

    Mise en place des vues

--- django_app/listings/models.py
+++ django_app/listings/models.py
@@ -3,4 +3,7 @@ from django.db import models
 class Band(models.Model):
     
     # Champ qui stocke les données de type chaine/caractère/texte dans une base de données
-    name = models.fields.CharField(max_length=100) 
+    name = models.fields.CharField(max_length=100)
+
+class Listings(models.Model):
+    title = models.fields.CharField(max_length=100)
--- django_app/listings/views.py
+++ django_app/listings/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from listings.models import Band, Listings
 
 #Pour créer une vue, il faut déclarer une fonction avec en parametre request
 def hello(request):
-    return HttpResponse("<h1> hELLO Django</h1>")
+    bands = Band.objects.all()
+    listings = Listings.objects.all()
+    
+    return HttpResponse(f"""
+                        <h1> hELLO Django</h1>
+                        <p>Mes groupes préférés sont : </p>
+                        <ul>
+                            <li>{bands[0].name} et {listings[0].title} </li>
+                            <li>{bands[1].name} et {listings[1].title}</li>
+                            <li>{bands[0].name} et {listings[2].title}</li>
+                        </ul>
+                        """)
 
 def about(request):
     return HttpResponse("<h1> A propos </h1> <p>Nous adorons Django</p>")
